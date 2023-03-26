e72d61451d0d1d90f749677e9bd2f8b18b8741cd
Konstantin Kostov <kosta.norge@gmail.com>
2023-03-26 08:29:04 +0300

    AJAX exercise

--- java_script_front_end/HTTP, REST,AJAX and Fetch Exercise/04.Blog/app.js
+++ java_script_front_end/HTTP, REST,AJAX and Fetch Exercise/04.Blog/app.js
@@ -1,5 +1,70 @@
 function attachEvents() {
-    // TODO:
+    document.querySelector('#btnLoadPosts').addEventListener('click', load)
+    document.querySelector('#btnViewPost').addEventListener('click', view)
+
+    const select = document.querySelector('#posts')
+    const postTitle = document.querySelector('#post-title')
+    const postBody = document.querySelector('#post-body')
+    const container = document.querySelector('#post-comments')
+    const content = []
+
+
+    async function load() {
+        const res = await fetch('http://localhost:3030/jsonstore/blog/posts')
+        const data = await res.json()
+
+        createOptions(data)
+    }
+
+    async function view() {
+        container.innerHTML = ''
+        const res = await fetch('http://localhost:3030/jsonstore/blog/comments')
+        const data = await res.json()
+
+        let option = Array
+        .from(select.querySelectorAll('option'))
+        .filter(opt => opt.selected)[0]
+
+        postTitle.textContent = option.textContent
+        
+        content.forEach(obj => {
+            if (obj.id === option.value) {
+                postBody.textContent = obj.body
+            }
+        })
+          
+        createComments(data, option)
+    }
+
+    function createOptions(data) {
+        Object
+        .entries(data)
+        .forEach(([key, value]) => {
+            let option = document.createElement('option')
+            option.value = key
+            option.textContent = value.title
+            select.appendChild(option)
+
+            content.push({
+                id: key,
+                body: value.body
+            })
+        })
+    }
+
+    function createComments(data, option) {
+
+        Object
+        .values(data)
+        .forEach(obj => {
+            if (obj.postId === option.value) {
+                let li = document.createElement('li')
+                li.textContent = obj.text
+                container.appendChild(li)
+            }
+        })
+    }
 }
 
 attachEvents();
+
--- java_script_front_end/HTTP, REST,AJAX and Fetch Exercise/05.Messenger/app.js
+++ java_script_front_end/HTTP, REST,AJAX and Fetch Exercise/05.Messenger/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
-    // TODO:
+    document.querySelector('#refresh').addEventListener('click', getAllMsg)
+    document.querySelector('#submit').addEventListener('click', sendMsg)
+
+    function sendMsg() {
+        const author = document.querySelector('input[name=author]')
+        const content = document.querySelector('input[name=content]')
+
+        if (author.value !== '' && content.value !== '') {
+            let body = {
+                author: author.value,
+                content: content.value
+            }
+    
+            author.value = ''
+            content.value = ''
+            createMsg(body)
+        }
+    }
+
+    async function createMsg(body) {
+        const BASE_URL = 'http://localhost:3030/jsonstore/messenger'
+        await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+    }
+
+    async function getAllMsg() {
+        const BASE_URL = 'http://localhost:3030/jsonstore/messenger'
+
+        const res = await fetch(BASE_URL)
+        const data = await res.json()
+
+        renderMsg(data)
+    }
+
+    function renderMsg(data) {
+        const textArea = document.querySelector('#messages')
+        content = Object.values(data)
+        .map(obj => `${obj.author}: ${obj.content}`)
+        .join('\n')
+        textArea.textContent = content
+    }
 }
 
 attachEvents();
