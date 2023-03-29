9e56cc154d67cf48267f397befbf926cc30471f6
Konstantin Kostov <kosta.norge@gmail.com>
2023-03-29 09:52:53 +0300

    examen preparation

--- java_script_front_end/Exam Preparation 1/task-3/solve.js
+++ java_script_front_end/Exam Preparation 1/task-3/solve.js
@@ -1,6 +1,110 @@
-// TODO
+
 function attachEvents() {
-  
+    const BASE_URL = 'http://localhost:3030/jsonstore/tasks/'
+    const input = document.querySelector('#title')
+
+    const btnLoad = document.querySelector('#load-button')
+    btnLoad.addEventListener('click', loadTasks)
+
+    const btnAdd = document.querySelector('#add-button')
+    btnAdd.addEventListener('click', addTask)
+
+    const todoList = document.querySelector('#todo-list')
+    todoList.addEventListener('click', removeTask)
+    todoList.addEventListener('click', editForm)
+    todoList.addEventListener('click', editTask)
+
+    async function loadTasks(ev) {
+        if (ev) {
+            ev.preventDefault()
+        }
+        todoList.innerHTML = ''
+        const res = await fetch(BASE_URL)
+        const data = await res.json()
+        Object.values(data).forEach(el => {
+     
+           const li = document.createElement('li')
+           const span = document.createElement('span')
+           const btnRemove = document.createElement('button')
+           const btnEdit = document.createElement('button')
+
+           li.id = el._id
+           span.textContent = el.name
+           btnRemove.textContent = 'Remove'
+           btnEdit.textContent = 'Edit'
+
+           li.appendChild(span)
+           li.appendChild(btnRemove)
+           li.appendChild(btnEdit)
+
+           todoList.appendChild(li)
+        })
+    }
+
+    async function addTask(ev) {
+        ev.preventDefault()
+        if (input.value == '') {
+            return
+        }
+        await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+               name: input.value
+            })
+        })
+
+        input.value = ''
+        loadTasks(ev)
+    }
+
+    async function removeTask(ev) {
+        if (ev.target.tagName === 'BUTTON' && ev.target.textContent === 'Remove') {
+            let id  = ev.target.parentElement.id
+            await fetch(`${BASE_URL}${id}`, {
+                method: 'DELETE'
+            })
+            ev.target.parentElement.remove()
+        }
+    }
+
+    function editForm(ev) {
+        if (ev.target.tagName === 'BUTTON' && ev.target.textContent === 'Edit') {
+            
+            let li = ev.target.parentElement
+            let span = li.querySelector('span')
+            li.innerHTML = `
+                <input type="text" value="${span.textContent}">
+                <button>Remove</button>
+                <button>Submit</button>`
+        }
+    }
+
+    async function editTask(ev) {
+        if  (ev.target.tagName === 'BUTTON' && ev.target.textContent === 'Submit') {
+            let li = ev.target.parentElement
+            let editInput = li.querySelector('input')
+            let text = editInput.value
+
+            await fetch(`${BASE_URL}${li.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: text
+                })
+            })
+
+            li.innerHTML = `
+            <span>${text}</span>
+            <button>Remove</button>
+            <button>Edit</button>`
+
+        }
+    } 
 }
 
 attachEvents();
---
+++ java_script_front_end/Exam Preparation 2/task-3/app.js
@@ -0,0 +1,132 @@
+function solve() {
+    let updateId = ''
+    const BASE_URL = 'http://localhost:3030/jsonstore/grocery/'
+    const productName = document.querySelector('#product')
+    const productCount = document.querySelector('#count')
+    const productPrice = document.querySelector('#price')
+
+    const btnAdd = document.querySelector('#add-product')
+    btnAdd.addEventListener('click', addProduct)
+
+    const btnUpdate = document.querySelector('#update-product')
+    btnUpdate.addEventListener('click',updateProduct)
+    
+    const btnLoad = document.querySelector('#load-product')
+    btnLoad.addEventListener('click', loadProduct)
+    
+    const tbody = document.querySelector('#tbody')
+    tbody.addEventListener('click', deleteProduct)
+    tbody.addEventListener('click', activateUpdateButton)
+
+    async function loadProduct(ev) {
+        if (ev) {
+            ev.preventDefault()
+        }
+        tbody.innerHTML = ''
+        const res = await fetch(BASE_URL)
+        const data = await res.json()
+        Object.values(data).forEach(({ product, count, price, _id }) => {
+        
+           const tr = document.createElement('tr')
+           const tdName = createEl('td', 'name', product, null, tr)
+           const tdCount = createEl('td', 'count-product', count, null, tr)
+           const tdPrice = createEl('td', 'product-price', price, null, tr)
+           const tdButtons = createEl('td', 'btn', null, _id, tr)
+           const upButton = createEl('button', 'update', 'Update', null, tdButtons)
+           const delButton = createEl('button', 'delete', 'Delete', null, tdButtons)
+
+           tbody.appendChild(tr)
+
+        })
+    }
+
+    async function addProduct(ev) {
+        ev.preventDefault()
+        await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                product: productName.value,
+                count: productCount.value,
+                price: productPrice.value
+            })
+       })
+       productName.value = ''
+       productCount.value = ''
+       productPrice.value = ''
+
+       loadProduct()
+    }
+
+    async function deleteProduct(ev) {
+        if (ev.target.tagName === 'BUTTON' && ev.target.textContent === 'Delete') {
+            let id = ev.target.parentElement.id
+            await fetch(`${BASE_URL}${id}`, {
+                method: 'DELETE'
+            })
+
+            ev.target.parentElement.parentElement.remove()
+        }
+    }
+
+    function activateUpdateButton(ev) {
+        
+        if (ev.target.tagName === 'BUTTON' && ev.target.textContent === 'Update') {
+            updateId = ev.target.parentElement.id
+            btnUpdate.disabled = false
+            btnAdd.setAttribute('disabled', true)
+
+            productName.value = ev.target.parentElement.parentElement.querySelector('td.name').textContent
+            productCount.value = ev.target.parentElement.parentElement.querySelector('td.count-product').textContent
+            productPrice.value = ev.target.parentElement.parentElement.querySelector('td.product-price').textContent
+            
+        }
+
+    }
+
+    async function updateProduct(ev) {
+        await fetch(`${BASE_URL}${updateId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                product: productName.value,
+                count: productCount.value,
+                price: productPrice.value
+            })
+        })
+
+        productName.value = ''
+        productCount.value = ''
+        productPrice.value = ''
+
+        btnUpdate.disabled = true
+        btnAdd.disabled = false
+        loadProduct()
+    }
+
+    function createEl(tag, _class, text, id, parent) {
+        let element = document.createElement(tag)
+        if (_class) {
+            element.classList.add(_class)
+        }
+        if (text) {
+            element.textContent = text
+        }
+        if (id) {
+            element.id = id
+        }
+
+        if (parent) {
+            parent.appendChild(element)
+        }
+
+        return element
+    }
+
+}
+
+solve()
