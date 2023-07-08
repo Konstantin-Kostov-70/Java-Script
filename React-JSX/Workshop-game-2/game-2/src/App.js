31b21ac20494c4e84af58e0901662c81c18bea1f
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2023-07-09 01:05:51 +0300

    create components

--- React-JSX/Workshop-game-2/game-2/src/App.js
+++ React-JSX/Workshop-game-2/game-2/src/App.js
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header/Header";
+import { HomePage } from "./components/HomePage/HomePage";
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div id="box">
       <Header />
       <main id="main-content">
+        <HomePage />
       </main>
     </div>
   );
