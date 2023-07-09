4c4b2a37d8e122ea3a22bf266f8ea0c25292e367
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2023-07-09 03:20:28 +0300

    create routes and replace a tag with link tag

--- React-JSX/Workshop-game-2/game-2/src/App.js
+++ React-JSX/Workshop-game-2/game-2/src/App.js
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { HomePage } from "./components/HomePage/HomePage";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { DetailsPage } from "./components/DetailsPage/DetailsPage";
+import { EditPage } from "./components/EditPage/EditPage";
+import { CreatePage } from "./components/CreatePage/CreatePage";
+import { Catalog } from "./components/Catalog/Catalog";
 
 
 function App() {
@@ -7,7 +14,15 @@ function App() {
     <div id="box">
       <Header />
       <main id="main-content">
-        <HomePage />
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/details/:id' element={<DetailsPage />}/>
+          <Route path='/edit/:id' element={<EditPage />}/>
+          <Route path='/create' element={<CreatePage />}/>
+          <Route path='/catalog' element={<Catalog />}/>
+        </Routes>
       </main>
     </div>
   );
