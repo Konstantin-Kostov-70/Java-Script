6b98190ba3211f12eb53a5f2576650a40847521d
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2023-07-09 04:18:43 +0300

    create all CRUD in views

--- React-JSX/Workshop-game-2/game_rest_server/game_rest_server/game_api/urls.py
+++ React-JSX/Workshop-game-2/game_rest_server/game_rest_server/game_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from game_rest_server.game_api.views import GamesListCreateView
+from game_rest_server.game_api.views import GamesListCreateView, GameGetEditDeleteView
 
 urlpatterns = [
-    path('games/', GamesListCreateView.as_view(), name='games')
+    path('games/', GamesListCreateView.as_view(), name='games'),
+    path('games/<int:pk>/', GameGetEditDeleteView.as_view(), name='single-game'),
 ]
+
--- React-JSX/Workshop-game-2/game_rest_server/game_rest_server/game_api/views.py
+++ React-JSX/Workshop-game-2/game_rest_server/game_rest_server/game_api/views.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,4 +14,37 @@ class GamesListCreateView(APIView):
         return Response(get_game_serializer.data)
 
     def post(self, request):
-        pass
+        post_game_serializer = GamesSerializer(data=request.data)
+        if post_game_serializer.is_valid():
+            post_game_serializer.save()
+            return Response(post_game_serializer.data, status=status.HTTP_200_OK)
+        return Response({'message': "not correct data"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GameGetEditDeleteView(APIView):
+    def get(self, request, pk):
+        try:
+            game = Games.objects.get(pk=pk)
+            get_game_serializer = GamesSerializer(game)
+            return Response(get_game_serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'message': 'not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        try:
+            game = Games.objects.get(pk=pk)
+            put_game_serializer = GamesSerializer(game, data=request.data)
+            if put_game_serializer.is_valid():
+                put_game_serializer.save()
+                return Response(put_game_serializer.data, status=status.HTTP_200_OK)
+            return Response({'message': put_game_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            return Response({'message': 'not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        try:
+            game = Games.objects.get(pk=pk)
+            game.delete()
+            return Response(status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'message': 'not found'}, status=status.HTTP_204_NO_CONTENT)
