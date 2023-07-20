96ba70bc7aa4725acf9a71fcfd405ae0e1e6071b
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2023-07-20 05:33:46 +0300

    create functionality in Headers

--- React-JSX/Workshop-game-2/game-2/src/components/Header/Header.js
+++ React-JSX/Workshop-game-2/game-2/src/components/Header/Header.js
@@ -1,21 +1,26 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/authContext'
 
-export const Header = () =>
-{
+export const Header = () => {
+    const {auth} = useContext(AuthContext)
     return (
         <header>
-        <h1><Link to={'/'} className="home" >GamesPlay</Link></h1>
-        <nav>
-            <Link to={'/catalog'}>All games</Link>
-            <div id="user">
-                <Link to={'/create'}>Create Game</Link>
-                <Link to={'/Logout'}>Logout</Link>
-            </div>
-            <div id="guest">
-                <Link to={'/login'}>Login</Link>
-                <Link to={'/register'}>Register</Link>
-            </div>
-        </nav>
-    </header>
+            <h1><Link to={'/'} className="home" >GamesPlay</Link></h1>
+            <nav>
+                {auth.token
+                ? <div id="user">
+                    <Link to={''}>user {auth.user_info.username}</Link>
+                    <Link to={'/catalog'}>All games</Link>
+                    <Link to={'/create'}>Create Game</Link>
+                    <Link to={'/Logout'}>Logout</Link>
+                  </div>
+                : <div id="guest">
+                    <Link to={'/login'}>Login</Link>
+                    <Link to={'/register'}>Register</Link>
+                  </div>
+                }
+            </nav>
+        </header>
     )
 }
