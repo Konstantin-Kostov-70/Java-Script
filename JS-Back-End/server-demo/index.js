5c7dd43d989a602101b2fc92e008b7295c547453
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2023-10-02 02:58:12 +0300

    start new Node.js project Workshop1

--- JS-Back-End/server-demo/index.js
+++ JS-Back-End/server-demo/index.js
@@ -8,12 +8,12 @@ const hbs = handlebars.create({
     extname: '.hbs',
 });
 
-app.use('/content', express.static('static'))
-app.use(express.urlencoded({extended: true}))
-
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 app.listen(3000);
+
+app.use('/content', express.static('static'))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', homeController);
 app.use('/catalog', catalogController);
