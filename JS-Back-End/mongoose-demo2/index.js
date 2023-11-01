93cc5d78f311bf4b2ebf2d609290babcfa4da709
kosta@gmail.com <kosta.norge@gmail.com>
2023-11-01 01:29:29 +0100

    mongoose demo2 initialize - mongoose, connection, string

---
+++ JS-Back-End/mongoose-demo2/index.js
@@ -0,0 +1,7 @@
+// [x] install mongoose and initialize then
+// [x] initialize connection string
+// [x] initialize database
+
+const mongoose = require(mongoose);
+const CONNECTION_STRING = 'mongodb://localhost:27017';
+const DATABASE_NAME = 'test1';
